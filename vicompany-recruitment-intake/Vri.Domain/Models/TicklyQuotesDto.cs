ff79ad1ee5f1742effdf49e7a71c47601f235549
ArtemStolbchenko <stolbchenko.artem@gmail.com>
2024-10-15 21:21:13 +0200

    Created TicklyQuotesDto
    It also contains a function to convert itself into a ready array of quotes
    The idea is to use this Data Transfer Object as bridge between Tickle's response and the Quotes within the system

---
+++ vicompany-recruitment-intake/Vri.Domain/Models/TicklyQuotesDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vri.Domain.Models
+{
+    public class TicklyQuotesDto
+    { // used to process responses from tickly.vicompany.io
+        public string Id { get; set; }
+
+        public List<List<string>> Ticks { get; set; }
+
+        public IReadOnlyList<Quote> ToQuotes()
+        {
+            var quotes = new List<Quote>();
+
+            foreach (var ticklyQuote in this.Ticks)
+            { // plain foreach loops should be faster than LINQ
+
+                var timeStamp = DateTimeOffset.FromUnixTimeMilliseconds(Convert.ToInt64(ticklyQuote[0])).LocalDateTime; // local time just as at Tickle
+                var tick = Convert.ToDecimal(ticklyQuote[1]); // second item is a tick
+
+                var quote = new Quote(timeStamp, tick);
+                quotes.Add(quote);
+            }
+
+            return quotes;
+        }
+    }
+}
